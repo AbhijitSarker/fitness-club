9b4277047469d15c2ca1c64d650998aa2093fa5f
AbhijitSarker <abhijitsarker03@gmail.com>
2023-07-26 19:45:05 +0600

    cards

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Exercise.css';
 
 const Exercise = ({ exercise }) => {
     const { name, age, time, about, company } = exercise;
     return (
-        <div>
-            hello
+        <div className='exercise'>
+            <img className='image' src={require('../../images/gym.jpg')} alt="fg" />
+            <div className='exercise-info'>
+                <h3>{name.slice(0, 300)}</h3>
+                <p>{about}</p>
+                <p>For Age: {age}</p>
+                <p>Time Required: {time}</p>
+
+            </div>
+
+            <button className='btn-cart'>
+                <p className='btn-text'>Add to cart</p>
+
+            </button>
         </div>
     );
 };
